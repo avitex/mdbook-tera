1f087960f28cc8060a2f45495a0940ff9277b1fa
avitex <theavitex@gmail.com>
2020-02-26 18:41:51 +1100

    better erroring

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,28 @@
 use mdbook::book::{Book, BookItem};
-use mdbook::errors::Error as BookError;
+use mdbook::errors::{Error as BookError, ErrorKind as BookErrorKind};
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
 use serde::Serialize;
-use tera::{Error as TeraError, Tera};
+use tera::Tera;
 use toml::Value as TomlValue;
+use error_chain::ChainedError;
+
+use self::errors::{Error, ErrorKind};
 
 pub use tera::Context;
 
-#[derive(Debug)]
-pub enum ProcessingError {
-    Tera(TeraError),
-    InvalidPath,
-}
+mod errors {
+    use error_chain::error_chain;
 
-impl From<TeraError> for ProcessingError {
-    fn from(err: TeraError) -> Self {
-        Self::Tera(err)
+    error_chain! {
+        foreign_links {
+            Tera(::tera::Error);
+        }
+
+        errors {
+            InvalidPath
+        }
+
+        skip_msg_variant
     }
 }
 
@@ -67,18 +74,14 @@ impl Preprocessor for TeraPreprocessor {
         ctx.insert("ctx", &book_ctx);
         ctx.extend(self.context.clone());
 
-        // TODO: handle errors
-        render_book_items(&mut book, &mut tera, &ctx).unwrap();
+        render_book_items(&mut book, &mut tera, &ctx)
+            .map_err(|err| BookErrorKind::Msg(err.display_chain().to_string()))?;
 
         Ok(book)
     }
 }
 
-fn render_book_items(
-    book: &mut Book,
-    tera: &mut Tera,
-    context: &Context,
-) -> Result<(), ProcessingError> {
+fn render_book_items(book: &mut Book, tera: &mut Tera, context: &Context) -> Result<(), Error> {
     let mut templates = Vec::new();
     // build the list of templates
     collect_item_chapters(&mut templates, book.sections.as_slice())?;
@@ -91,11 +94,14 @@ fn render_book_items(
 fn collect_item_chapters<'a>(
     templates: &mut Vec<(&'a str, &'a str)>,
     items: &'a [BookItem],
-) -> Result<(), ProcessingError> {
+) -> Result<(), Error> {
     for item in items {
         match item {
             BookItem::Chapter(chapter) => {
-                let path = chapter.path.to_str().ok_or(ProcessingError::InvalidPath)?;
+                let path = chapter
+                    .path
+                    .to_str()
+                    .ok_or(Error::from(ErrorKind::InvalidPath))?;
                 templates.push((path, chapter.content.as_str()));
                 collect_item_chapters(templates, chapter.sub_items.as_slice())?;
             }
@@ -109,11 +115,14 @@ fn render_item_chapters(
     tera: &mut Tera,
     context: &Context,
     items: &mut [BookItem],
-) -> Result<(), ProcessingError> {
+) -> Result<(), Error> {
     for item in items {
         match item {
             BookItem::Chapter(chapter) => {
-                let path = chapter.path.to_str().ok_or(ProcessingError::InvalidPath)?;
+                let path = chapter
+                    .path
+                    .to_str()
+                    .ok_or(Error::from(ErrorKind::InvalidPath))?;
                 chapter.content = tera.render(path, context)?;
                 render_item_chapters(tera, context, chapter.sub_items.as_mut_slice())?;
             }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,64 @@
-// use clap::{App, Arg, ArgMatches, SubCommand};
+use std::io;
+use std::process;
 
-// fn app() -> App {
-//     App::new("tera-preprocessor")
-//         .about("A mdbook preprocessor that renders")
-//         .subcommand(
-//             SubCommand::with_name("supports")
-//                 .arg(Arg::with_name("renderer").required(true))
-//                 .about("Check whether a renderer is supported by this preprocessor"),
-//         )
-// }
+use clap::{App, Arg, ArgMatches, SubCommand};
+use mdbook::errors::Error;
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
 
-// fn main() {
-//     let matches = app().get_matches();
+use mdbook_tera::TeraPreprocessor;
 
-//     if let Some(sub_args) = matches.subcommand_matches("supports") {
-//         handle_supports(&preprocessor, sub_args);
-//     } else if let Err(e) = handle_preprocessing(&preprocessor) {
-//         eprintln!("{}", e);
-//         process::exit(1);
-//     }
-// }
+fn app() -> App<'static, 'static> {
+    App::new("tera-preprocessor")
+        .about("A mdbook preprocessor that renders tera")
+        .subcommand(
+            SubCommand::with_name("supports")
+                .arg(Arg::with_name("renderer").required(true))
+                .about("Check whether a renderer is supported by this preprocessor"),
+        )
+}
 
-// fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
-//     let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+fn main() {
+    let matches = app().get_matches();
 
-//     if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
-//         // We should probably use the `semver` crate to check compatibility
-//         // here...
-//         eprintln!(
-//             "Warning: The {} plugin was built against version {} of mdbook, \
-//              but we're being called from version {}",
-//             pre.name(),
-//             mdbook::MDBOOK_VERSION,
-//             ctx.mdbook_version
-//         );
-//     }
+    let preprocessor = TeraPreprocessor::from_json_str(r#"{"users": ["jam"]}"#);
 
-//     let processed_book = pre.run(&ctx, book)?;
-//     serde_json::to_writer(io::stdout(), &processed_book)?;
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        handle_supports(&preprocessor, sub_args);
+    } else if let Err(e) = handle_preprocessing(&preprocessor) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
 
-//     Ok(())
-// }
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
 
-fn main() {}
+    if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
+        // We should probably use the `semver` crate to check compatibility
+        // here...
+        eprintln!(
+            "Warning: The {} plugin was built against version {} of mdbook, \
+             but we're being called from version {}",
+            pre.name(),
+            mdbook::MDBOOK_VERSION,
+            ctx.mdbook_version
+        );
+    }
 
-// fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
-//     let renderer = sub_args.value_of("renderer").expect("Required argument");
-//     let supported = pre.supports_renderer(&renderer);
+    let processed_book = pre.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book)?;
 
-//     // Signal whether the renderer is supported by exiting with 1 or 0.
-//     if supported {
-//         process::exit(0);
-//     } else {
-//         process::exit(1);
-//     }
-// }
+    Ok(())
+}
+
+fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
+    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let supported = pre.supports_renderer(&renderer);
+
+    // Signal whether the renderer is supported by exiting with 1 or 0.
+    if supported {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
